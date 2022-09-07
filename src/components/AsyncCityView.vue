f41d249225e4ed2c31a7d439997774cf924327e1
Suman Kathet <sumankathet51@gmail.com>
2022-09-07 10:06:03 +0545

    get weather Data

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -1,17 +1,29 @@
 <template>
-  <div></div>
+  <div class="flex flex-col flex-1 items-center">
+    <!-- banner -->
+    <div
+      v-if="route.query.preview"
+      class="text-white p-4 bg-weather-secondary w-full text-center"
+    >
+      <p>
+        You are currently viewing the city, click the "+" icon to start tracking
+        this city.
+      </p>
+    </div>
+    <!-- Weather Overview -->
+  </div>
 </template>
 
 <script setup>
 import axios from "axios";
-import useRoute from "vue-router";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
-const apiKey = "82402898394dcbf6b480dc45f66830e4";
-const getWeatherDate = async () => {
+const apiKey = "65ebdc3a1d6321c369b018aff1788348";
+const getWeatherData = async () => {
   try {
     const weatherData = await axios.get(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=${apiKey}&units=imperial`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=${apiKey}&units=imperial`
     );
 
     const localOffset = new Date().getTimezoneOffset() * 60000;
@@ -20,11 +32,16 @@ const getWeatherDate = async () => {
       utc + 1000 * weatherData.data.timezone_offset;
 
     weatherData.data.hourly.forEach((hour) => {
-      const utc = hour.current.dt * 1000 + localOffset;
+      const utc = hour.dt * 1000 + localOffset;
       hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
     });
+
+    return weatherData.data;
   } catch (err) {
     console.log(err);
   }
 };
+
+const weatherData = await getWeatherData();
+console.log(weatherData);
 </script>
