85a244a43a3efb107747436df2fdaf31697d6729
Suman Kathet <sumankathet51@gmail.com>
2022-09-07 12:39:09 +0545

    display weather data in the UI

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -11,6 +11,69 @@
       </p>
     </div>
     <!-- Weather Overview -->
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+      <p class="text-sm mb-12">
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString("en-us", {
+            weekday: "short",
+            day: "2-digit",
+            month: "long",
+          })
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString("en-us", {
+            timeStyle: "short",
+          })
+        }}
+      </p>
+      <p class="text-8xl mb-8">
+        {{ Math.round(weatherData.current.temp) }}&deg;
+      </p>
+
+      <p>
+        Feels like
+        {{ Math.round(weatherData.current.feels_like) }}&deg;
+      </p>
+      <p class="capitalize">
+        {{ weatherData.current.weather[0].description }}
+      </p>
+      <img
+        :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
+        class="w-[150px] h-auto"
+        alt=""
+      />
+    </div>
+
+    <hr class="border-white border-opacity-10 border w-full" />
+
+    <!-- Hourly Weather -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">Hourly Weather</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div
+            v-for="hourData in weatherData.hourly"
+            :key="hourData.dt"
+            class="flex flex-col gap-4"
+          >
+            <p class="whitespace-nowrap text-md">
+              {{
+                new Date(hourData.currentTime).toLocaleTimeString("en-us", {
+                  hour: "numeric",
+                })
+              }}
+            </p>
+            <img
+              :src="`http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png`"
+              class="h-[50px] w-auto object-cover"
+              alt=""
+            />
+            <p class="text-xl">{{ Math.round(hourData.temp) }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,5 +106,4 @@ const getWeatherData = async () => {
 };
 
 const weatherData = await getWeatherData();
-console.log(weatherData);
 </script>
